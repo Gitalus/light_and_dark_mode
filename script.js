6354c4db304532d457472870261206252977b0ec
Italo Guajardo <italog.penaloza@gmail.com>
2021-08-19 14:38:19 -0400

    add dark mode functionality to toggle button

---
+++ script.js
@@ -0,0 +1,15 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
+function switchTheme(event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+};
+
+
+
+// Event Listeners
+toggleSwitch.addEventListener('change', switchTheme);
